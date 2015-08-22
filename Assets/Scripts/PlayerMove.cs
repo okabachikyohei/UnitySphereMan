2efbd2cd1b401cd2f591a32f5b58088a2576b0cd
姜兵 <jiang@kyouhei-no-MacBook-Pro.local>
2015-08-22 13:12:03 +0900

    add walk animation

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	public GameObject player;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveSpeed = Input.GetAxis ("Vertical");
+		Debug.Log(moveSpeed);
+
+	}
+}
